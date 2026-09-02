4fd2154071ac41a81edb9dd34a03a62f1ab13bf3
Leogalazzo <113796961+Leogalazzo@users.noreply.github.com>
2026-09-02 01:12:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Todo lo que va a Firebase, Cloudinary, Google Fonts, etc. se deja pasar
 // directo a la red: los datos del panel siempre tienen que estar frescos.
 
-const CACHE_NAME = 'ce-admin-cache-v35';
+const CACHE_NAME = 'ce-admin-cache-v5';
 
 const APP_SHELL = [
     '/admin',
